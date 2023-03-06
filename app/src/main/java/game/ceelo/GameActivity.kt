19d5d5848dca93677945cb86b42e5c4e9b053dc6
cheroliv <40507040+cheroliv@users.noreply.github.com>
2023-03-06 20:48:39 +0100

    refactor rename and clean code.

--- app/src/main/java/game/ceelo/GameActivity.kt
+++ app/src/main/java/game/ceelo/GameActivity.kt
@@ -2,14 +2,11 @@ package game.ceelo
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import game.ceelo.controller.binding
-import game.ceelo.vm.GameViewModel
 import org.koin.androidx.viewmodel.ext.android.getViewModel
 
 class GameActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        getViewModel<GameViewModel>().loadLocalGame(this, binding)
+        getViewModel<GameViewModel>().loadLocalGame(this)
     }
 }
